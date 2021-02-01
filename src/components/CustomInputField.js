479c5e960cc85580a224f3f6370f3245b096db7a
vlshugai2 <vlshugai@gmail.com>
2021-02-01 12:36:52 +0200

    fix custom input

--- src/components/CustomInputField.js
+++ src/components/CustomInputField.js
@@ -6,7 +6,7 @@ export default class CustomInputField extends Component {
     render() {
         
         return (
-            <input className="custom-input-field" checked disabled = {this.props.disabled} placeholder={this.props.placeholder} type={this.props.type} />
+            <input className="custom-input-field" disabled = {this.props.disabled} placeholder={this.props.placeholder} type={this.props.type} />
         )
     }
 }
