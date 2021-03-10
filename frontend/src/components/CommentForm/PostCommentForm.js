0bd992cbce57739c5992ab7241f906083671e6c8
Shuga69 <54189758+Shuga69@users.noreply.github.com>
2021-03-10 17:38:37 +0300

    add success message for post comment and fix encoding

--- frontend/src/components/CommentForm/PostCommentForm.js
+++ frontend/src/components/CommentForm/PostCommentForm.js
@@ -3,22 +3,82 @@ import CustomInputField from "../ContactForm/CustomInputField";
 import SectionSubtitle from "../SectionSubtitle/SectionSubtitle";
 import Button from "../Button/Button";
 import './PostCommentForm.css'
+import axios from "axios";
 
 class PostCommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: "",
+            secondName: "",
+            text: "",
+            successMessage: false,
+            showModal: props.show
+        };
+
+    }
+    handleSubmit(e){
+        e.preventDefault();
+
+        axios.post('https://monovex-shuhai.herokuapp.com/comment/addComment', this.state,
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((response)=>{
+                this.setState({
+                    successMessage:true
+                });
+                setTimeout(() => {
+                    this.setState({
+                        successMessage:false,
+                        showModal:false
+                    });
+                }, 5000);
+            })
+    }
+    setInputValue(event,inputName){
+        if(event.target.name===inputName){
+            this.setState({
+                [inputName]:event.target.value
+                })
+            };
+
+        }
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            showModal: nextProps.show
+        });
+    }
     render() {
-        if(!this.props.show){
+
+        if(!this.state.showModal){
             return null;
         }
-        return (
+       else if(this.state.successMessage){
 
-            <div className="post-comment-container">
-                <SectionSubtitle title="Залиште свій відгук"/>
-                <CustomInputField type="text" placeholder="Ім'я"/>
-                <CustomInputField type="text" placeholder="Прізвище"/>
-                <textarea cols="40" placeholder="Відгук"></textarea>
-                <Button text="Відправити"/>
-            </div>
-        );
+            return (<div className="user-response">
+                <p>Відгук успішно відправлений</p>
+            </div>)
+        }
+       else if(this.props.show) {
+            return (
+                <form className="comment-post-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <div className="post-comment-container">
+                        <SectionSubtitle title="Залиште свій відгук"/>
+                        <CustomInputField type="text" onChange={event => this.setInputValue(event, "firstName")}
+                                          name="firstName" placeholder="Ім'я"/>
+                        <CustomInputField type="text" onChange={event => this.setInputValue(event, "secondName")}
+                                          name="secondName" placeholder="Прізвище"/>
+                        <textarea cols="40" name="text" onChange={event => this.setInputValue(event, "text")}
+                                  placeholder="Відгук"/>
+                        <Button text="Відправити"/>
+                    </div>
+                </form>
+            );
+        }
     }
 }
 
