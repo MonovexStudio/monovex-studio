cd51629dcf5789a0e79634854cf443cf7345d760
DimasCoder <dimakulykovych@gmail.com>
2021-03-12 16:06:30 +0200

    Navbar drop down when scroll up

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,34 +7,45 @@ import {faPhone} from '@fortawesome/free-solid-svg-icons'
 import CircleIcon from "../CircleIcon/CircleIcon";
 
 
-const Header = props => (
-    <header className="header">
-        <div className="container">
-            <div className="header__inner">
-                <img
-                    src={logo}
-                    className="header__inner-logo"
-                    alt="Logo"
-                />
-                <nav className="header-navigation">
-                    <Link to="services" smooth={true} className="header__navigation-link">Послуги</Link>
-                    <Link to="motivation" smooth={true} className="header__navigation-link">Переваги</Link>
-                    <Link to="reviews" smooth={true} className="header__navigation-link">Відгуки</Link>
-                    <Link to="portfolio" smooth={true} className="header__navigation-link">Наші проекти</Link>
-                    <Link to="order" smooth={true} className="header__navigation-link">Замовити</Link>
-                </nav>
-                <div className="header__inner-contact">
-                    <a className="contact-phone" href="tel:0999990999"><CircleIcon icon={faPhone}/> &nbsp; 098 411 8945</a>
-                    <a className="contact-phone" href="tel:0999990999"><CircleIcon icon={faPhone}/> &nbsp; 098 411 8945</a>
-                    {/*<a className="contact-email" href="#">monovex.studio@gmail.com</a>*/}
-                </div>
-                <div className="header__inner-toggle">
-                    <ToggleButton click={props.drawerClickHandler} toggle={props.toggle}/>
+const Header = props => {
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector("header").style.top = "0";
+        } else {
+            document.querySelector("header").style.top = "-72px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+    return(
+        <header className="header">
+            <div className="container">
+                <div className="header__inner">
+                    <img
+                        src={logo}
+                        className="header__inner-logo"
+                        alt="Logo"
+                    />
+                    <nav className="header-navigation">
+                        <Link to="services" smooth={true} className="header__navigation-link">Послуги</Link>
+                        <Link to="motivation" smooth={true} className="header__navigation-link">Переваги</Link>
+                        <Link to="reviews" smooth={true} className="header__navigation-link">Відгуки</Link>
+                        <Link to="portfolio" smooth={true} className="header__navigation-link">Наші проекти</Link>
+                        <Link to="order" smooth={true} className="header__navigation-link">Замовити</Link>
+                    </nav>
+                    <div className="header__inner-contact">
+                        <a className="contact-phone" href="tel:0999990999"><CircleIcon icon={faPhone}/> &nbsp; 098 411 8945</a>
+                        <a className="contact-phone" href="tel:0999990999"><CircleIcon icon={faPhone}/> &nbsp; 098 411 8945</a>
+                        {/*<a className="contact-email" href="#">monovex.studio@gmail.com</a>*/}
+                    </div>
+                    <div className="header__inner-toggle">
+                        <ToggleButton click={props.drawerClickHandler} toggle={props.toggle}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-
-)
+        </header>
+    )
+}
 
 export default Header;
