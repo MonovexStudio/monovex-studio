da6cf379fc0c04ade868077bec50a1459b9c4dd6
Shuga69 <54189758+Shuga69@users.noreply.github.com>
2021-03-19 16:51:55 +0300

    build project, change comment

--- frontend/src/components/CommentForm/Comment.js
+++ frontend/src/components/CommentForm/Comment.js
@@ -5,18 +5,22 @@ class Comment extends Component {
     render() {
         return (
             <div className="comment">
+                <div className="info-container">
+                    {/*<div className="credentials">*/}
+                        <div className="avatar-circle">
+                            <span className="avatar">{this.props.avatar}</span>
+                        </div>
+                        <div className="info-text">
+                        <p>{this.props.credentials}</p>
+                        <p className="comment-date">{this.props.date}</p>
+                        </div>
+                    {/*</div>*/}
+
+                </div>
                      <div className="text-container bubble-bottom-left" contenteditable>
                          {this.props.text}
                      </div>
-                <div className="info-container">
-                    <div className="credentials">
-                        <div className="avatar-circle">
-                        <span className="avatar">{this.props.avatar}</span>
-                        </div>
-                    <p>{this.props.credentials}</p>
-                    </div>
-                    <p>{this.props.date}</p>
-                </div>
+
             </div>
         );
     }
