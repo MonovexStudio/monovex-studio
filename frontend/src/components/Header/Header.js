90321b2d649b958754231377ba6ed8c473bcde24
DimasCoder <dimakulykovych@gmail.com>
2021-03-25 15:03:46 +0200

    Test new header hide on Scroll

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Header.css";
 import logo from '../../assets/mono-logo.png';
 import ToggleButton from '../ToggleButton/ToggleButton'
@@ -8,23 +8,84 @@ import CircleIcon from "../CircleIcon/CircleIcon";
 import {NavLink} from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
+import {debounce} from "../../utilities/helper";
 
 const Header = props => {
     const { t } = useTranslation();
-    let prevScrollpos = window.pageYOffset;
     const [language, setLanguage] = useState('uk');
 
-    window.onscroll = function() {
-        let currentScrollPos = window.pageYOffset;
+   /* window.onscroll = function() {
+        let curScroll;
+        let prevScroll = window.scrollY || document.scrollTop;
+        let curDirection = 0;
+        let prevDirection = 0;
+        let header = document.querySelector("header")
+        let toggled;
+        let threshold = 200;
+
+
+        curScroll = window.scrollY || document.scrollTop;
+        if(curScroll > prevScroll) {
+            // scrolled down
+            curDirection = 2;
+        }
+        else {
+            //scrolled up
+            curDirection = 1;
+        }
+
+        if(curDirection !== prevDirection) {
+            toggled = toggleHeader();
+        }
+
+        prevScroll = curScroll;
+        if(toggled) {
+            prevDirection = curDirection;
+        }
+
+        var toggleHeader = () => {
+            toggled = true;
+            if(curDirection === 2 && curScroll > threshold) {
+                header.classList.add('hide');
+            }
+            else if (curDirection === 1) {
+                header.classList.remove('hide');
+            }
+            else {
+                toggled = false;
+            }
+            return toggled;
+        };
+        /*let currentScrollPos = window.pageYOffset;
         let header = document.querySelector("header")
         if (prevScrollpos > currentScrollPos) {
-            header.style.top = "50px";
+            header.style.top = "0";
         } else {
             header.style.top = "-72px";
             header.style.position = "fixed"
         }
         prevScrollpos = currentScrollPos;
-    }
+    }*/
+
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    const handleScroll = debounce(() => {
+        const currentScrollPos = window.pageYOffset;
+
+        setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10);
+
+        setPrevScrollPos(currentScrollPos);
+    }, 100);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+
+    }, [prevScrollPos, visible, handleScroll]);
+
+
 
 
     function handleChange(e) {
@@ -34,7 +95,7 @@ const Header = props => {
         i18next.changeLanguage(lang);
     }
     return(
-        <header className="header">
+        <header style={{top: visible ? 0 : '-72px'}} className="header">
             <div className="container">
                 <div className="header__inner">
                     <NavLink className="brand-image" to="/">
